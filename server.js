d38efcad20480c5f75fbc8011100d9ee6893f9fe
mwonlinecourses <mwonlinecourses@gmail.com>
2017-02-12 18:41:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ app.get('/article-one',function (req, res){
 });
 
 app.get('/article-two',function (req, res){
-    res.send("Article two web page will be handled here!");
+    res.sendFile(path.join(__dirname,"ui","article-two.html"));
 });
 app.get('/article-three',function (req, res){
-    res.send("Article three web page will be handled here!");
+     res.sendFile(path.join(__dirname,"ui","article-three.html"));
 });
 
 
