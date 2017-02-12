1a81599f02e327261d18b2f03bba08b565465937
mwonlinecourses <mwonlinecourses@gmail.com>
2017-02-13 00:48:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ var articles ={
 
 function createTemplate (data){
 
-var title = data.tile;
+var title = data.title;
 var heading = data.heading;
 var date = data.date;
 var content = data.content;
