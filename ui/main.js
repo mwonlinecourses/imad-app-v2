44d4dfcb3cbfc95caca2fa7d28cd57a5f948a540
mwonlinecourses <mwonlinecourses@gmail.com>
2017-02-15 00:12:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
 //Changes to be done on the main text
-var element = document.getElemenmtById('main-text');
+var element = document.getElementById('main-text');
 
 element.innerHTML='New Value';
