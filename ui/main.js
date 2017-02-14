8b5b46236a4d9391cf690b0ae19e83d8b28b1c66
mwonlinecourses <mwonlinecourses@gmail.com>
2017-02-15 00:24:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,13 @@ element.innerHTML='New Value';
 //move image 
 var img= document.getElementById('madi');
 
+marginleft=0;
+function moveRight(){
+    marginleft = marginleft + 10;
+    img.style.marginLeft = marginleft+'px'; 
+}
+
 img.onclick = function(){
-    img.style.marginLeft='100px';
+    var interval = setInterval(moveRight,100);
+   // img.style.marginLeft='100px';
 };
