88fd509e951dd805d6dc9f995b6afe5f3b88da33
mwonlinecourses <mwonlinecourses@gmail.com>
2017-02-19 20:28:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,25 @@
 var button = document.getElementById('counter');
 var counter = 0;
-console.log('JS');
 button.onclick = function (){
   
   counter = counter + 1;
-  console.log("bjchjw");
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+  
+  
+    //create request
+    var request = new XMLHttpRequest ();
+    
+    request.onreadystatechange = function (){
+      if(request.readyState === XMLHttpRequest.DONE){
+          if(request.status === 200){
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+              
+          }
+      }  
+    };
+    
+    request.open('GET','http://mwonlinecourses.hasura-app.io/counter',true);
+    request.send();
     
 };
