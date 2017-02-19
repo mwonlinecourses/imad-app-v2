d69be18169ff4ea050c13779f52e1c7ed684f38f
mwonlinecourses <mwonlinecourses@gmail.com>
2017-02-19 22:04:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,8 +124,13 @@ app.get('/:articleName',function (req, res){
     res.send(createTemplate(articles[articleName]));
 });
 
-
-
+var names[];
+app.get('/submit-name/:name' , function (req,res){
+  var name=req.params.name;
+  names.push(name);
+  
+  res.send(JSON.stringfy(names));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
