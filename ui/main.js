1026d486b85619b8a30825a2932f17ac29b97dc0
mwonlinecourses <mwonlinecourses@gmail.com>
2017-02-19 23:27:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -55,3 +55,30 @@ submit.onclick = function(){
   request.send(null);
   
 };
+
+//comments JS
+var commentbutton = document.getElementById(add-comment);
+commentbutton.onclick = function() {
+  var commentInput = document.getElementById(comment);
+  var comment = commentInput.value;
+  
+  var request = new XMLHttpRequest();
+  
+  request.onreadystatechange = function() {
+    if(request.readyState === XMLHttpRequest.DONE){
+        if(request.status === 200){
+            var comments = request.responseText;
+            comments = JSON.parse(comments);
+            var li='';
+            for(var i=0;i < comments.length ; i++){
+                li += '<li>'+comments[i]+'</li>';
+            }
+            var commentlist = document.getElementById('commentlist');
+            commentlist.innerHTML = li;
+        }
+    }  
+  };
+  
+  request.open('GET','https://mwonlinecourses.imad.hasura-app.io/add-comment?currentcomment='+comment,true);
+  request.send(null);
+};
