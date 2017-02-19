8ee9803731516f9b7f773d3e8de79ef98c3ee86e
mwonlinecourses <mwonlinecourses@gmail.com>
2017-02-19 22:35:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,6 +38,8 @@ submit.onclick = function(){
       if(request.readyState === XMLHttpRequest.DONE){
            if(request.status === 200){
             //var names = ['name1','name2','name3','name4'];
+            var names = request.responseText;
+            names = JSON.parse(names);
              var li='';
                for(var i=0 ; i<names.length ; i++){
                   li += '<li>'+names[i]+'</li>';
