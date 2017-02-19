527ee2bd430cf77bfce04ff9736f33b0f6de098f
mwonlinecourses <mwonlinecourses@gmail.com>
2017-02-19 22:32:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,17 +26,32 @@ button.onclick = function (){
 
 //submit
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 
 var submit = document.getElementById('search_button');
 submit.onclick = function(){
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    
+    
+    var names = [];
+    var request = new XMLHttpRequest()
   
-  var names = ['name1','name2','name3','name4'];  
-  var li='';
-  for(var i=0 ; i<names.length ; i++){
-      li += '<li>'+names[i]+'</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = li;
+    request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE){
+           if(request.status === 200){
+            //var names = ['name1','name2','name3','name4'];
+             var li='';
+               for(var i=0 ; i<names.length ; i++){
+                  li += '<li>'+names[i]+'</li>';
+              }
+              var ul = document.getElementById('namelist');
+              ul.innerHTML = li; 
+        }   
+     }  
+  };
+    
+  request.open('GET','http://mwonlinecourses.imad.hasura-app.io/submit-name?name='+name,true);
+  request.send(null);
+  
 };
