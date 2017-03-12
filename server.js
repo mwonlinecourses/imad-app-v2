455c7d50dcd378d5be98aa8a21efd4320bd27dbe
mwonlinecourses <mwonlinecourses@gmail.com>
2017-03-12 14:55:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -167,9 +167,19 @@ app.get('/submit-comment', function(req,res){
     res.send(JSON.stringify(comments));
 });
 
-app.get('/:articleName',function (req, res){
-    var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName',function (req, res){
+    
+    
+    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else if(result.rows.length === 0){
+            res.status(404).send('Article does not exist');
+        } else {
+            var articleData = result.rows[0];
+        }
+    });
+    res.send(createTemplate(articleData));
 });
 
 
