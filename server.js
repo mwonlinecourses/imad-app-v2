fd04ea45c6ce7a1423c7d6657e4e4a1877e547fa
mwonlinecourses <mwonlinecourses@gmail.com>
2017-03-18 18:43:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
   username : 'mwonlinecourses',
@@ -69,6 +70,7 @@ return htmlTemplate;
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -83,6 +85,23 @@ function hash(input, salt){
 app.get('/hash/:input', function(req, res){
    var hashedString = hash(req.params.input,'This-is-some-random-string');
    res.send(hashedString);
+});
+
+app.post('/create-user', function(req, res){
+    
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    var salt = crypto.getRandomBytes(128).toString();
+    var dbString= hash(password, salt); 
+    
+    pool.query('INSERT into "USER" (username, password) VALUES ($1, $2)', [username, dbString], function(err, result){
+      if(err){
+           res.status(500).send(err.toString());
+       } else{
+           res.send('User Successfully cfeated : ' + username);
+       }  
+    });
 });
 
 var pool = new Pool(config);
