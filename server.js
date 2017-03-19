793656a352cda96ded6bdd0c2bec18ac771af5ad
mwonlinecourses <mwonlinecourses@gmail.com>
2017-03-19 19:03:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,8 +73,8 @@ var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
-   secret : 'someRandomSecretValue' ,
-   cookie : { maxAge: 1000 * 60 * 60 * 24 * 30} 
+   secret: 'someRandomSecretValue' ,
+   cookie: { maxAge: 1000 * 60 * 60 * 24 * 30} 
 }));
 
 app.get('/', function (req, res) {
