2b183f0008b18871a67e52c74f4a8877f9a3763f
mwonlinecourses <mwonlinecourses@gmail.com>
2017-03-19 17:50:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,6 +104,19 @@ app.post('/create-user', function(req, res){
     });
 });
 
+app.post('/login', function (req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    pool.query('SELECT * FROM "user" username = $1', username, function(err, result){
+      if(err){
+           res.status(500).send(err.toString());
+       } else{
+           res.send('User Successfully cfeated : ' + username);
+       }  
+    });
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function(req, res){
     pool.query('SELECT * FROM test', function(err, result){
