10dc450104a8d5eb0801b2e8b069c89d1f85a624
mwonlinecourses <mwonlinecourses@gmail.com>
2017-03-19 18:26:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,58 +1,34 @@
-var button = document.getElementById('counter');
-var counter = 0;
-button.onclick = function (){
-  
-  counter = counter + 1;
-  
-  
-    //create request
-    var request = new XMLHttpRequest ();
-    
-    request.onreadystatechange = function (){
-      if(request.readyState === XMLHttpRequest.DONE){
-          if(request.status === 200){
-              var counter = request.responseText;
-              var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-              
-          }
-      }  
-    };
-    
-    request.open('GET','http://mwonlinecourses.imad.hasura-app.io/counter',true);
-    request.send(null);
-    
-};
 
-//submit
+//submit username/password
 
 
-var submit = document.getElementById('search_button');
+var submit = document.getElementById('submit_button');
 submit.onclick = function(){
     
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
+    
     
     var request = new XMLHttpRequest()
   
     request.onreadystatechange = function() {
       if(request.readyState === XMLHttpRequest.DONE){
            if(request.status === 200){
-            //var names = ['name1','name2','name3','name4'];
-            var names = request.responseText;
-            names = JSON.parse(names);
-             var li='';
-               for(var i=0 ; i<names.length ; i++){
-                  li += '<li>'+names[i]+'</li>';
-              }
-              var ul = document.getElementById('namelist');
-              ul.innerHTML = li; 
-        }   
+            console.log('user is loged in ');
+            alert('Logged in Successfully');
+        }   else if(request.status === 403){
+            alert('Username/ password is invalid');
+        } else if(request.status === 500){
+            alert('Something went wrong on server');
+        }
      }  
   };
-    
-  request.open('GET','http://mwonlinecourses.imad.hasura-app.io/submit-name?name='+name,true);
-  request.send(null);
+   
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  console.log(username);
+  console.log(password);
+  request.open('POST','http://mwonlinecourses.imad.hasura-app.io/login', true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify({username : username, password : password}));
   
 };
 
