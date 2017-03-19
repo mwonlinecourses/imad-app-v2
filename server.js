b997517d2e3af294e0786b36723fc915083dba8d
mwonlinecourses <mwonlinecourses@gmail.com>
2017-03-19 18:00:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,11 +108,27 @@ app.post('/login', function (req, res){
     var username = req.body.username;
     var password = req.body.password;
     
-    pool.query('SELECT * FROM "user" username = $1', username, function(err, result){
+    pool.query('SELECT * from "user" username = $1', [username], function(err, result){
       if(err){
            res.status(500).send(err.toString());
        } else{
-           res.send('User Successfully cfeated : ' + username);
+           if(result.rows.length === 0){
+               res.send(403).send('Username/Password Invalid');
+           }else{
+               //match the password
+               var dbString = result.rows[0].password;
+               var salt = dbString.split('$')[2];
+               
+               var hashedPassword = hash(password, salt);
+               
+               if(hashedPassword === dbString){
+                   res.send('Credentials Correct !');
+               }else{
+                   res.send(403).send('Username/Password Invalid');
+               }
+               
+           }
+           
        }  
     });
 });
